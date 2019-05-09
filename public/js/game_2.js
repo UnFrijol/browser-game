5498e18d619bdddb80ee4d7f647df9a925cd9899
Victor Beltran-Gil <victorbeltrangil@gmail.com>
2019-05-09 19:36:14 +0200

    Add title scene test files

---
+++ public/js/game_2.js
@@ -0,0 +1,11 @@
+let titleScene = new TitleScene();
+
+let config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 360
+};
+
+let game = new Phaser.Game(config);
+game.scene.add('TitleScene', titleScene);
+game.scene.start('TitleScene');
