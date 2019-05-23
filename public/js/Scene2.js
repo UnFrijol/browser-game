bf56c5d4bd5c2e66d3582386b531ca50caf54301
Victor Beltran-Gil <victorbeltrangil@gmail.com>
2019-05-23 21:43:17 +0200

    Fix beam. Add colliders and overlaps

--- public/js/Scene2.js
+++ public/js/Scene2.js
@@ -11,6 +11,11 @@ class Scene2 extends Phaser.Scene {
         this.ship2 = this.add.sprite(config.width / 2, config.height / 2, 'ship2');
         this.ship3 = this.add.sprite(config.width / 2 + 50, config.height / 2, 'ship3');
 
+        this.enemies = this.physics.add.group();
+        this.enemies.add(this.ship1);
+        this.enemies.add(this.ship2);
+        this.enemies.add(this.ship3);
+
         this.powerUps = this.physics.add.group();
 
         let maxObjects = 4;
@@ -48,6 +53,29 @@ class Scene2 extends Phaser.Scene {
         this.player.setCollideWorldBounds(true);
 
         this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        this.projectiles = this.add.group();
+
+        this.physics.add.collider(this.projectiles, this.powerUps, (projectile, powerUp) => {
+            projectile.destroy();
+        });
+        this.physics.add.overlap(this.player, this.powerUps, this.pickPowerUp, null, this);
+        this.physics.add.overlap(this.player, this.enemies, this.hurtPlayer, null, this);
+        this.physics.add.overlap(this.projectiles, this.enemies, this.hitEnemy, null, this);
+    }
+
+    pickPowerUp(player, powerUp) {
+        powerUp.disableBody(true, true);
+    }
+
+    hurtPlayer(player, enemy) {
+        this.resetShipPos(enemy);
+        player.x = config.width / 2 - 8;
+        player.y = config.height - 64;
+    }
+
+    hitEnemy(projectile, enemy) {
+        projectile.destroy();
+        this.resetShipPos(enemy);
     }
 
     moveShip(ship, speed) {
@@ -80,19 +108,32 @@ class Scene2 extends Phaser.Scene {
         if (Phaser.Input.Keyboard.JustDown(this.spacebar)) {
             this.shootBeam();
         }
+
+        for (let i = 0; i < this.projectiles.getChildren().length; i++) {
+            let beam = this.projectiles.getChildren()[i];
+            beam.update();
+        }
     }
 
     movePlayerManager() {
         if (this.cursorKeys.left.isDown) {
-            this.player.setVelocityX(-gameSettings.playerSpeed);
+            this.player.setVelocityX(- gameSettings.playerSpeed);
         } else if (this.cursorKeys.right.isDown) {
             this.player.setVelocityX(gameSettings.playerSpeed);
         }
 
         if (this.cursorKeys.up.isDown) {
-            this.player.setVelocityY(-gameSettings.playerSpeed);
+            this.player.setVelocityY(- gameSettings.playerSpeed);
         } else if (this.cursorKeys.down.isDown) {
             this.player.setVelocityY(gameSettings.playerSpeed);
+        }
+
+        if (!this.cursorKeys.left.isDown && !this.cursorKeys.right.isDown) {
+            this.player.setVelocityX(0);
+        }
+
+        if (!this.cursorKeys.up.isDown && !this.cursorKeys.down.isDown) {
+            this.player.setVelocityY(0);
         }
     }
 
--- public/js/beam.js
+++ public/js/beam.js
@@ -4,6 +4,17 @@ class Beam extends Phaser.GameObjects.Sprite {
         let y = scene.player.y;
 
         super(scene, x, y, 'beam');
+        scene.add.existing(this);
         scene.projectiles.add(this);
+        
+        this.play('beam_anim');
+        scene.physics.world.enableBody(this);
+        this.body.velocity.y = - 250;
+    }
+
+    update() {
+        if (this.y < 32) {
+            this.destroy();
+        }
     }
 }
